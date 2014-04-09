c84faf9e9c5cb6596e8d61c7b601465010cfde07
allisonburtch <allisonburtch@gmail.com>
2014-04-09 16:53:31 -0400

    removed test brands

--- test/test-brands.js
+++
@@ -1,9 +0,0 @@
-var brands = require('../data/brands.js');
-
-exports["test replace"] = function(assert) {
-  var observed = brands.replace('hsaoehoeu aoe  oeht Lightlife tnoaetnsoeu');
-  var expected = 'hsaoehoeu aoe  oeht Lightlife (ConAgra) tnoaetnsoeu';
-  assert.equal(observed, expected);
-};
-
-require("sdk/test").run(exports);
